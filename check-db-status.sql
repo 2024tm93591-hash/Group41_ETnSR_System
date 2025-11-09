b3204b48fb2d4b7b8b40fd9120d67bdbfaad41fd
2024tm93591-hash <2024tm93591@wilp.bits-pilani.ac.in>
2025-11-10 00:58:40 +0530

    Remove all files from main branch as requested

--- check-db-status.sql
+++
@@ -1,5 +0,0 @@
--- Database Status Check Script
-USE catalog_db;
-SELECT 'catalog_db' as Database, 'Events' as TableName, COUNT(*) as RowCount FROM Events
-UNION ALL 
-SELECT 'catalog_db', 'Venues', COUNT(*) FROM Venues;
